22a11be11395dd63196a6beb1499766a14f43572
Varsha C S <varsha.cs@carvewing.com>
2025-04-27 20:19:19 +0530

    adding naturalnumbers file

---
+++ 1naturalnum.py
@@ -0,0 +1,11 @@
+# Sum of n natural numbers using while loop
+
+n=int(input("Enter the n value: "))
+sum=0
+count=0
+
+while n>=count:
+    sum=sum+count
+    count+=1
+
+print("The sum of first n natural numbers is: ",sum)
